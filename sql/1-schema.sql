babfb36c08e88a7be1dfdcba7848199903f15cf7
isucon <isucon@localhost>
2026-07-29 00:27:38 +0000

    coupons.used_byにインデックス追加（フルスキャン解消）

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -137,6 +137,7 @@ CREATE TABLE coupons
   discount   INTEGER      NOT NULL COMMENT '割引額',
   created_at DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '付与日時',
   used_by    VARCHAR(26)  NULL COMMENT 'クーポンが適用されたライドのID',
-  PRIMARY KEY (user_id, code)
+  PRIMARY KEY (user_id, code),
+  INDEX idx_used_by (used_by)
 )
   COMMENT 'クーポンテーブル';
