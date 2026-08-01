00449ad12255b2d8cbae6414682edc455d94f04b
tomatommy-bs <tomatommy.bs@gmail.com>
2026-08-01 22:24:27 +0900

    fix: astro.config.mjsのbaseに末尾スラッシュを追加してリンク欠損を修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomatommy-bs.github.io',
-  base: '/isucon14',
+  base: '/isucon14/',
 });
